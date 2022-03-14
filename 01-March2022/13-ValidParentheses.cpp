9d37870b70b1b4ffe37abc844aafa7141529bf83
Amit Singh Rana <ranaa73000@gmail.com>
2022-03-14 22:22:41 +0530

    13-March-2022   'Valid Parenthese'   --EASY--

---
+++ 01-March2022/13-ValidParentheses.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        
+        for(char ch : s) {
+            switch (ch) {
+                case ')':
+                    if(!st.empty()  &&  st.top() == '(')
+                        st.pop();
+                    else
+                        return false;
+                    break;
+                case '}':
+                    if(!st.empty()  &&  st.top() == '{')
+                        st.pop();
+                    else
+                        return false;
+                    break;
+                case ']':
+                    if(!st.empty()  &&  st.top() == '[')
+                        st.pop();
+                    else
+                        return false;
+                    break;
+                default:
+                    st.push(ch);
+            }
+        }
+        
+        return st.empty();
+    }
+};
