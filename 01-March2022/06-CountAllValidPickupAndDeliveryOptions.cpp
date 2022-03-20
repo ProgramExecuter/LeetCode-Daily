4d59837ae75aa029ac2934ea2f7651c5890906ff
Amit Singh Rana <ranaa73000@gmail.com>
2022-03-20 13:21:28 +0530

    20-March-2022   'Count All Valid Pickup And Delivery Options'   --HARD

---
+++ 01-March2022/06-CountAllValidPickupAndDeliveryOptions.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countOrders(int n) {
+        long res = 1, mod = 1e9+7;
+        
+        for(int i = 1; i <= n; ++i) {
+            res *= (2*i - 1)*i;
+            res %= mod;
+        }
+        
+        return res;
+    }
+};
