11052f0f07ec7666ed2d7d8c00ad13825690b735
Amit Singh Rana <ranaa73000@gmail.com>
2022-03-29 12:19:03 +0530

    29-March-2022   'Find the Duplicate Number'   --MEDIUM--

---
+++ 01-March2022/29-FindTheDuplicateNumber.cpp
@@ -0,0 +1,24 @@
+// Time Complexity - O(n)
+// Space Complexity - O(n)
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = 0, fast = 0;
+        
+        // Linked List cycle detection type solution
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow != fast);
+        
+        fast = 0;
+        
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        
+        return slow;
+    }
+};
