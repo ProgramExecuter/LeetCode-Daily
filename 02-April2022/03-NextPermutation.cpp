f51fd69895100d862ae181d3c9a4795e42aaf751
Amit Singh Rana <ranaa73000@gmail.com>
2022-04-03 12:42:11 +0530

    03-April-2021   'Next Permutation'   --MEDIUM--

---
+++ 02-April2022/03-NextPermutation.cpp
@@ -0,0 +1,33 @@
+// Time Complexity - O(NlogN)
+// Space Complexity - O(1)
+
+class Solution {
+public:
+    void nextPermutation(vector<int> &nums) {
+        int len = nums.size(), pos = -1;
+        
+        // Find the pos where the element is not in ascending order from back
+        for(int i = len-2; i >= 0; --i) {
+            if(nums[i] < nums[i+1]) {
+                pos = i;
+                break;
+            }
+        }
+        
+        if(pos < 0) {
+            goto xy;
+        }
+        
+        // Find and replace the 'pos' element with
+        // element just greater than it from right side of 'pos'
+        for(int i = len-1; i > pos; --i) {
+            if(nums[i] > nums[pos]) {
+                swap(nums[i], nums[pos]);
+                break;
+            }
+        }
+        
+        xy:
+        sort(nums.begin()+pos+1, nums.end());
+    }
+};
